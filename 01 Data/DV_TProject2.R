37f744e9be308798f18db467f9c01b0e49c43019
Ronly Leung <ronlyleung@yahoo.com>
2015-11-13 13:44:05 -0600

    DV_TProject2

---
+++ 01 Data/DV_TProject2.R
@@ -0,0 +1,36 @@
+require("jsonlite")
+require("RCurl")
+require(ggplot2)
+require(dplyr)
+
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select POP, REAL_GDP, YEAR, COUNTRY
+  from globaleconomics
+  where REAL_GDP is not NULL
+;"
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ryl96', PASS='orcl_ryl96', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); 
+
+df<-dplyr::filter(df, POP < 266859)
+View(df)
+
+ggplot() + 
+  geom_point() +
+  labs(title='Real GDP Versus Population') +
+  labs(x=paste("Pop"), y=paste("Real GDP")) +
+  layer(data=df, 
+        mapping=aes(x=POP, y=REAL_GDP, color=YEAR, label=COUNTRY), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_identity()
+  ) +
+  layer(data=df, 
+        mapping=aes(x=POP, y=REAL_GDP, color=YEAR, label=COUNTRY), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(), 
+        position=position_identity(),
+        
+  ) 
