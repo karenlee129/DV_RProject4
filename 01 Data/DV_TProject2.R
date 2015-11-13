8d303c069605ce35852a4f47adfb0ce796fd6cae
Ronly Leung <ronlyleung@yahoo.com>
2015-11-13 16:43:41 -0600

    another attempt

--- 01 Data/DV_TProject2.R
+++ 01 Data/DV_TProject2.R
@@ -4,23 +4,24 @@ require(ggplot2)
 require(dplyr)
 
 KPI_Low_Max_value = 1930.22     
-KPI_Medium_Max_value = 7108.52
+KPI_Medium_Max_value = 4108.52
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-"select country, real_gross_domestic_income, year,
+"select country, real_gross_domestic_income, year, kpi as ratio,
 case
 when kpi < "p1" then \\\'03 Low\\\'
 when kpi < "p2" then \\\'02 Medium\\\'
 else \\\'01 High\\\'
 end kpi  
 from (select country, real_gross_domestic_income, year, sum(real_gross_domestic_income) as kpi from globaleconomics group_by(country))
+where real_gross_domestic_income is not NULL
 ;"
 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_ryl96', PASS='orcl_ryl96', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value, verbose = TRUE))); 
 
 df<-dplyr::filter(df, YEAR > 2000)
 df<-dplyr::filter(df, YEAR < 2010)
 
-
+df <- df %>% mutate(ratio = sum(REAL_GROSS_DOMESTIC_INCOME)) %>% mutate(KPI = ifelse(ratio <= KPI_Low_Max_value, '03 Low', ifelse(ratio <= KPI_Medium_Max_value, '02 Medium', '01 High')))
 
 View(df)
 
