ff93bbc8f0ea061b2d59fc05f20e268fa27c37bf
Karen Lee <karenlee129@utexas.edu>
2015-11-13 22:51:07 -0600

    edited rmd

--- 01 Data/blended_data.R
+++ 01 Data/blended_data.R
@@ -4,12 +4,11 @@ require(dplyr)
 
 global_economics_1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
 "select * from global_economics;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kwl377', PASS='orcl_kwl377', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
-View(global_economics_1)
+
 
 
 global_economics_2 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
 "select * from global_economics_2;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kwl377', PASS='orcl_kwl377', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)));
-View(global_economics_2)
 
 
 dplyr::inner_join(global_economics_1, global_economics_2, by="COUNTRY") %>% View
@@ -20,4 +19,4 @@ from global_economics_1 one join global_economics_2 two
 on one.country = two.country
 group by COUNTRY"
 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kwl377', PASS='orcl_kwl377', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-View(blended)
+
